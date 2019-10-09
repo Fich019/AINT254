1d5adda47c5b4f79edb9636b6583e67066510dbf
Fich019 <44195783+Fich019@users.noreply.github.com>
2019-10-09 01:50:08 +0100

    tweaks

---
+++ Homework/Assets/Scipts/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public float moveSpeed = 100.0f;
+
+    public GameObject Spawn;
+    public GameObject Prefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(1) == true)
+        {
+            Instantiate(Prefab);
+            Prefab.transform.position = Spawn.transform.position;
+        }
+    }
+}
