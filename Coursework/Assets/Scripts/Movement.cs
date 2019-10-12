50829d7f263b741a493e324fec8521ecb9e77c14
Fich019 <44195783+Fich019@users.noreply.github.com>
2019-10-12 11:54:49 +0100

    Jump

---
+++ Coursework/Assets/Scripts/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float jumpHeight = 2f;
+
+    public Vector3 jump;
+    private float x;
+    private GameObject player;
+    public bool isGrounded;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = this.gameObject;
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, jumpHeight, 0.0f);
+    }
+
+    private void OnCollisionStay()
+    {
+        isGrounded = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal") * speed;
+        player.transform.Translate(x, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded == true){
+            rb.AddForce(jump * jumpHeight, ForceMode.Impulse);
+            isGrounded = false;
+        }
+
+    }
+}
