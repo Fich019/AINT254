0d816fac57bc8fc83f94f777899d81cac1cc267d
Fich019 <44195783+Fich019@users.noreply.github.com>
2019-10-13 12:23:09 +0100

    Camera tweaks

---
+++ Coursework/Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float distance, height;
+    public float xMin, xMax;
+    public float yMin, yMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Clamp(player.position.x, xMin, xMax);
+        float y = Mathf.Clamp(player.position.y, yMin, yMax);
+
+        transform.position = new Vector3(x, height, -distance);
+
+    }
+}
