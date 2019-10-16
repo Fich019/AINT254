2de8ab09a5bf8ab573bccb25c1a1751a33c50cc5
Fich019 <44195783+Fich019@users.noreply.github.com>
2019-10-16 14:07:37 +0100

    Restart game on death

---
+++ Coursework/Assets/restartScene.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class restartScene : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision hit)
+    {
+        string main = SceneManager.GetActiveScene().name;
+
+        
+        if (hit.transform.tag == "Player")
+        {
+            SceneManager.LoadScene(main, LoadSceneMode.Single);
+            //print("memes");
+        }
+    }
+}
