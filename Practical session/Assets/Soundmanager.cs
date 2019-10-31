42fdda9193b0af472016d2eeb929eabd0a454466
Fich019 <44195783+Fich019@users.noreply.github.com>
2019-10-31 12:39:32 +0000

    lession

---
+++ Practical session/Assets/Soundmanager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Soundmanager : MonoBehaviour
+{
+    [SerializeField]
+    private AudioMixer m_audioMixer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        m_audioMixer.SetFloat("AmbianceVol", volume);
+    }
+
+    public void SetMusicVolume(Slider slider)
+    {
+        m_audioMixer.SetFloat("AmbianceVol", slider.value);
+    }
+}
