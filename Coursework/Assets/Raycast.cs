9777e610fbe0d99577f36bbdae8ad0e82b57f3f1
Fich019 <44195783+Fich019@users.noreply.github.com>
2019-11-21 00:55:25 +0000

    tweaks

--- Coursework/Assets/Raycast.cs
+++ Coursework/Assets/Raycast.cs
@@ -11,7 +11,7 @@ public class Raycast : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        raycam.transform.position = new Vector3(0,0,0);
+        
     }
 
     // Update is called once per frame
